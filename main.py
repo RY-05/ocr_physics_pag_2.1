f4ea272e7c2e21b072f81ec83bbec444a502d63c
RY <126905892+RY-05@users.noreply.github.com>
2023-04-29 12:55:15 +0100

    Update main.py

--- main.py
+++ main.py
@@ -12,34 +12,56 @@ extension_m = []
 for i in extension_mm:
     extension_m.append(i*(10**-3))
 
-print(extension_mm)
-print(extension_m)
+avg_ext_m = sum(extension_m) / len(extension_m)
 
-strain_perc = []
-for i in extension_m:
-    strain_perc.append(100 * i / LENGTH_m)
 
 strain = []
-for i in strain_perc:
-    strain.append(i / 100)
+for i in extension_m:
+    strain.append(i / LENGTH_m)
+
+strain_perc = []
+for i in strain:
+    strain_perc.append(str(i * 100) + '%')
+
 
 stress = []
 for i in mass_kg:
     # stress equals force applied over surface area
     stress.append((i * grav_field_str) / (np.pi * ((DIAMETER_m / 2) ** 2)))
 
-# Young's modulus
 
+# Young's modulus
 vals = 0
 for i in range(len(stress)):
     vals += stress[i] / strain[i]
 
 young_mod = vals / len(stress)
 
-print("Young's modulus is equal to " + str(young_mod) + " for a copper wire")
+print(f"Young's modulus is equal to {young_mod}.")
+print(f"Strain values are {strain_perc}.")
+print(f"Stress values are {stress}.")
 
 
 # stress-strain graph
-
-plt.plot(strain_perc, stress)
+plt.plot(strain, stress)
 plt.show()
+
+
+# uncertainties
+
+# natural length uncertainty, half of a millimetre
+length_unc_m = 0.0005
+
+
+# area uncertainty, based on diameter, 1/20 of a millimetre
+area_unc_m2 = 0.000005 ** 2
+
+
+# extension uncertainty, half a millimetre
+ext_unc_m = 0.0005
+
+
+print(f"The uncertainty is {young_mod} plus / minus {(length_unc_m + area_unc_m2 + ext_unc_m)}.")
+print(f"The percentage uncertainty is \
+{((length_unc_m / LENGTH_m) + (area_unc_m2 / DIAMETER_m) + (ext_unc_m / avg_ext_m)) * 100}.")
+
